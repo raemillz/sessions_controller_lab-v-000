b3b056b3785801393bb3314135e5638f4c2949c9
Rachel Miller <raeanne1991@gmail.com>
2018-04-12 20:29:17 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+  def create
+    session[:username] = params[:username]
+    redirect_to '/'
+  end
+
+  def new
+    
+  end
+
+  def destroy
+    session.delete :username
+  end
 end
